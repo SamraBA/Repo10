962870f48d9584c68fce58a3a69280896958d1f1
SamraBA <bandicsamra@gmail.com>
2021-09-23 00:52:49 +0200

    objects and some  html

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+
+var myPet = {
+    species: "Pomeranac",
+    name: "Fox",
+    legs: 4,
+    friends: "Puffy, Lexi"
+};
+
+function myFunction(myObj) {
+    return myObj;
+}
+
+// Only change code above this line
+
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
---
+++ ex10/src/index.js
@@ -0,0 +1,40 @@
+   
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            release_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002: {
+            artist: "Celine Dion",
+            title: "Courage",
+            release_year: 2019,
+            id: 100,
+            formats: {
+                1: "CD",
+                2: "DVD",
+                3: "LP"
+            },
+            gold: true
+            
+// Add a record here
+
+    }
+  };
+
+return myMusic;
+
+}
+
+myFunction()[2];
+
+module.exports = myFunction;
+
+
