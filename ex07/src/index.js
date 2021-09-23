14f97ff2003e5264d0a73abbb3faf4896151b599
SamraBA <90388771+SamraBA@users.noreply.github.com>
2021-09-23 10:20:23 +0200

    Update index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -3,12 +3,12 @@
 
 // Only change code below this line
 
-var lion = {
-    name: "Simba",
-    legs: 4,
-    tails: 1,
-    roar: "roar-roar"
-};
+  var lion = {
+        "name": "Simba",
+        "legs": 4,
+        "tails": 1,
+        "roar": "roar-roar"
+    };
 delete lion[propName];
 
 return lion;
